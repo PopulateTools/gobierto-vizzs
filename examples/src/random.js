31421c03c067de3094819a3a90fca63e5a0610dc
Hugoren Martinako <aumpfbahn@gmail.com>
2021-10-01 16:28:30 +0200

    tree shaking faker
    remove locales

--- examples/src/random.js
+++ examples/src/random.js
@@ -1,17 +1,17 @@
-import Faker from "faker";
+import { datatype, address, vehicle, date, lorem, random } from "faker/locale/en";
 
 export function mock() {
-  const groups = Array.from({ length: Faker.datatype.number({ min: 3, max: 9 }) }, () => Faker.address.country())
-  const relations = Array.from({ length: Faker.datatype.number({ min: 1500, max: 5000 }) }, () => Faker.vehicle.manufacturer())
+  const groups = Array.from({ length: datatype.number({ min: 3, max: 9 }) }, () => address.country());
+  const relations = Array.from({ length: datatype.number({ min: 1500, max: 5000 }) }, () => vehicle.manufacturer());
   return Array.from({ length: 100 }, (_, i) => ({
-    date: Math.random() > 0.98 ? null : Faker.date.past(),
-    value: Math.random() > 0.9 ? Faker.datatype.number(1e5) : Faker.datatype.number(1e4),
-    title: Faker.lorem.words(5),
-    relation: Faker.random.arrayElement(relations),
-    group: Faker.random.arrayElement(groups),
-    group2: Faker.random.arrayElement(groups),
-    group3: Faker.random.arrayElement(groups),
-    group4: Faker.random.arrayElement(groups),
+    date: Math.random() > 0.98 ? null : date.past(),
+    value: Math.random() > 0.9 ? datatype.number(1e5) : datatype.number(1e4),
+    title: lorem.words(5),
+    relation: random.arrayElement(relations),
+    group: random.arrayElement(groups),
+    group2: random.arrayElement(groups),
+    group3: random.arrayElement(groups),
+    group4: random.arrayElement(groups),
     id: i,
-  }))
+  }));
 }
