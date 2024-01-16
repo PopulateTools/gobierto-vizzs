3e107a47c52f2f84dc33cb474020d17302555401
Hugoren Martinako <aumpfbahn@gmail.com>
2024-01-16 18:08:58 +0100

    migrate faker

--- examples/src/random.js
+++ examples/src/random.js
@@ -1,29 +1,29 @@
 import { faker } from "@faker-js/faker";
 
-const { datatype, address, vehicle, date, lorem, random, commerce } = faker
+const { location, vehicle, date, lorem, helpers, commerce, number } = faker
 
 export function mockJSON(length = 100) {
-  const groups = Array.from({ length: datatype.number({ min: 3, max: 9 }) }, () => address.country());
-  const relations = Array.from({ length: datatype.number({ min: 10, max: 15 }) }, () => vehicle.manufacturer());
+  const groups = Array.from({ length: number.int({ min: 3, max: 9 }) }, () => location.country());
+  const relations = Array.from({ length: number.int({ min: 10, max: 15 }) }, () => vehicle.manufacturer());
   const phases = Array.from({ length: 20 }, () => commerce.department())
   const addDays = (d, n) => new Date(new Date(d).setDate(d.getDate() + n))
 
   return Array.from({ length }, (_, i) => ({
     date: Math.random() > 0.98 ? null : date.past(),
-    value: Math.random() > 0.9 ? datatype.number(1e5) : datatype.number(1e4),
-    value2: Math.random() > 0.9 ? datatype.number(1e5) : datatype.number(1e4),
-    value3: Math.random() > 0.9 ? datatype.number(1e5) : datatype.number(1e4),
-    value4: datatype.number(1e5),
+    value: Math.random() > 0.9 ? number.int(1e5) : number.int(1e4),
+    value2: Math.random() > 0.9 ? number.int(1e5) : number.int(1e4),
+    value3: Math.random() > 0.9 ? number.int(1e5) : number.int(1e4),
+    value4: number.int(1e5),
     title: lorem.words(5),
-    relation: random.arrayElement(relations),
-    group: random.arrayElement(groups),
+    relation: helpers.arrayElement(relations),
+    group: helpers.arrayElement(groups),
     id: i
   })).reduce((acc, x) => {
     const prev = acc.filter(d => d.relation === x.relation)
     const from = prev.length === 0
-        ? date.soon(30)
+        ? date.soon({ days: 30 })
         : addDays(prev[prev.length - 1].to, 0)
-    const to = addDays(from, datatype.number(100))
+    const to = addDays(from, number.int(100))
 
     return  [...acc, { ...x, phase: phases[prev.length], from, to }]
   }, []);
